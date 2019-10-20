c42b1e4522ea1263eaa7b65cc6984119b17e6544
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-10-20 16:48:52 -0400

    Update client side code

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -13,14 +13,96 @@ import java.util.Scanner;
 
 public class Client {
 	private static Socket socket;
+	
+	private static String ipAddress = "";
+	private static int portNumber = 0;
 	// /Users/felix-antoinebourbonnais/Documents/A2019/inf3405/myfile.txt
+	
+	public void checkIpAddress(Scanner input) {
+		try {
+			
+			System.out.println("Please enter the server's IP address: ");
+			input.nextLine();
+			
+			for (;;) {
+				ipAddress = input.nextLine();
+				
+				if (validIP(ipAddress)) {
+					break;
+				}
+				
+				System.out.println("The IP address must be composed of 4 bytes separated by periods.");
+				System.out.println("Please enter a valid IP address: ");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("The IP address must be composed of 4 bytes separated by periods. "
+					+ "Please restart the application and try again.");
+		}
+		System.out.println("Valid IP address entered. \n");
+	}
+	
+	public boolean validIP (String ip) {
+	    try {
+	        if ( ip == null || ip.isEmpty() ) {
+	            return false;
+	        }
+
+	        String[] parts = ip.split( "\\." );
+	        if ( parts.length != 4 ) {
+	            return false;
+	        }
+
+	        for ( String s : parts ) {
+	            int i = Integer.parseInt( s );
+	            if ( (i < 0) || (i > 255) ) {
+	                return false;
+	            }
+	        }
+	        if ( ip.endsWith(".") ) {
+	            return false;
+	        }
+
+	        return true;
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
+	}
+	
+	public void checkPortNumber(Scanner input) {
+		try {
+			
+			System.out.println("Please enter the port number you would like to use: ");
+			
+			for (;;) {
+				portNumber = input.nextInt();
+				
+				if (portNumber >= 5000 && portNumber <= 5050) {
+					break;
+				}
+				
+				System.out.println("The port number must be an integer between 5000 and 5050.");
+				System.out.println("Please enter a valid port number you would like to use: ");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("The port number must be an integer between 5000 and 5050. Please try again.");
+		}
+		System.out.println("Valid port number entered. \n");
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
-		String SERVER_ADDRESS = "127.0.0.1";
-		int PORT = 5000;
+		Client client = new Client();
+		Scanner input = new Scanner(System.in);
 		
-		socket = new Socket(SERVER_ADDRESS, PORT);
+		System.out.println("Hello! Welcome to your new favorite file manager! \n");
+		client.checkIpAddress(input);
+		client.checkPortNumber(input);
 		
-		System.out.format("The server is runnin on %s:%d%n", SERVER_ADDRESS, PORT);
+		socket = new Socket(ipAddress, portNumber);
+		
+		System.out.format("The server is running on %s:%d%n", ipAddress, portNumber);
 		
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		
@@ -30,11 +112,10 @@ public class Client {
 		String command = "";
 		OutputStream outputStream = socket.getOutputStream();
 		DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-		Scanner scanner = new Scanner(System.in);
 		
 		while (!command.equals("exit")) {
 		    System.out.println("Enter command");
-		    command = scanner.nextLine();
+		    command = input.nextLine();
 		    String[] commands = command.split(" ", 2);
 		    
 		    if (commands[0].equals("upload")) {
@@ -67,7 +148,7 @@ public class Client {
 	        // System.out.println(message);
 		}
 		
-		scanner.close();
+		input.close();
 		dataOutputStream.close();
 		
 		socket.close();
